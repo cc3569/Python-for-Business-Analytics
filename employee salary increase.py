acae840d996e99057e5305fddafa01c0107aba64
Cole T. Catron, B.S., B.S.B.A. <cc3569@nau.edu>
2024-09-19 16:40:28 -0700

    Update employee salary increase.py

--- employee salary increase.py
+++ employee salary increase.py
@@ -1,4 +1,4 @@
-#Dictionary defining employee IDs, their names, and salaries
+# Dictionary defining employee IDs, their names, and salaries
 employees = {
     601001: {"Cassandra": 60500},
     601002: {"Steffany ": 68000},
@@ -6,25 +6,25 @@ employees = {
     601004: {"Ashley   ": 105000}
 }
 
-#Variable initalizing salary increase of 8% annually
+# Variable initalizing salary increase of 8% annually
 salary_increase = 0.08
 
-#Prints current employee information
+# Prints current employee information
 print("EMPLOYEE INFORMATION\n")
 print("ID\tName\t   Salary")
 
-#Nested for loop iterates through nested dictionary. Prints current salaries
-#and employee information
+# Nested for loop iterates through nested dictionary. Prints current salaries
+# and employee information
 for i, j in employees.items():
   for k,l in j.items():
     print(f"{i}\t{k}  ${l}")
 
-#Prints updated employee information
+# Prints updated employee information
 print("\nUPDATED EMPLOYEE INFORMATION\n*Salary Increase of 8% Has Been Applied*\n")
 print("ID\tName\t   Salary")
 
-#Nested for loop iterates through nested dictionary, updating salaries with
-#increases and prints them to the consoles.
+# Nested for loop iterates through nested dictionary, updating salaries with
+# increases and prints them to the consoles.
 for i, j in employees.items():
   for k,l in j.items():
     l *= (1 + salary_increase)
